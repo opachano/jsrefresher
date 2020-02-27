d89f715569d59a80ce42f0448b57af81f39c6af1
Oscar Pachano <opachano@gmail.com>
2020-02-27 01:14:17 -0500

    Variables and arithmetics

---
+++ basics1.js
@@ -0,0 +1,31 @@
+// This is how you write in-line comments
+
+/* This is how you write
+multi-line comments */
+
+// ================================================================================================
+
+// Data Types in JavaScript
+
+var isNumber = 1;
+var isUndefined = undefined;
+var isNull = null;
+var isBoolean = true;
+var isString = "String";
+var isSymbol = Symbol("Test Symbol");
+var isObject = {they: "are", all: "objects"};
+
+/* Variable names can be made up of numbers, letters, and $ or _, 
+but may not contain spaces or start with a number. 
+
+JavaScript Arithmetic Operators
+===============================
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement */
+
